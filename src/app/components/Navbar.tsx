451f97f55bcf6cd3049ab4b784ed0dab247e7670
epenflow <epenflow@gmail.com>
2024-01-04 04:07:29 +0800

    responsive nav#1 error

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { IoIosMenu, IoMdClose } from 'react-icons/io';
 import React from 'react';
 type TNavlist = {
 	name: string;
@@ -15,9 +16,17 @@ const navlist: TNavlist[] = [
 ];
 const Navbar = () => {
 	const pathName = usePathname();
+	const [isActive, setActive] = React.useState<boolean>(false);
+
 	return (
-		<nav className='bg-black h-[75px] flex items-center justify-center p-2'>
-			<ul className='flex flex-row gap-4 capitalize text-sm font-light'>
+		<nav
+			className='bg-black flex lg:justify-center p-2'
+			style={
+				isActive
+					? { height: '100vh' }
+					: { height: '75px', alignItems: 'center' }
+			}>
+			<ul className='lg:flex hidden flex-row gap-4 capitalize text-sm font-light'>
 				{navlist.map((list, index) => {
 					return (
 						<li key={index}>
@@ -34,6 +43,42 @@ const Navbar = () => {
 					);
 				})}
 			</ul>
+			<div className='lg:hidden text-gray-600 flex flex-col w-full'>
+				<button
+					className='flex items-center'
+					style={
+						isActive
+							? {
+									height: 'calc(75px - 8px)',
+									alignItems: 'center',
+							  }
+							: {}
+					}
+					onClick={() => setActive((prev) => !prev)}>
+					{isActive ? (
+						<IoMdClose size={25} />
+					) : (
+						<IoIosMenu size={25} />
+					)}
+				</button>
+				{isActive ? (
+					<ul className='flex justify-center flex-col items-center pt-36 capitalize text-2xl'>
+						{navlist.map((list, index) => (
+							<li key={index}>
+								<Link
+									href={list.href}
+									className={
+										pathName === list.href
+											? `text-white`
+											: `text-gray-600`
+									}>
+									{list.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				) : null}
+			</div>
 		</nav>
 	);
 };
