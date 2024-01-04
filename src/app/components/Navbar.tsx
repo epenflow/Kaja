256a8aa5379197be84ce15b0a7f02eb2f250b656
epenflow <epenflow@gmail.com>
2024-01-04 13:56:31 +0800

    multilevel mobile navbar with dynamic data

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -76,10 +76,26 @@ const DesktopNav = ({
 	return (
 		<nav className='bg-black flex lg:justify-center p-2 h-[75px] items-center'>
 			<ul className='lg:flex hidden flex-row gap-4 capitalize text-sm font-light'>
-				{navlist.map((list, index) => {
-					if (!list.child) {
-						return (
-							<li key={index}>
+				{navlist.map((list, index) =>
+					!list.child ? (
+						<li key={index}>
+							<Link
+								href={list.href}
+								className={
+									pathName === list.href
+										? `text-white`
+										: `text-gray-600`
+								}>
+								{list.name}
+							</Link>
+						</li>
+					) : (
+						<div
+							key={index}
+							onMouseEnter={() => mouseEnter(index)}
+							onMouseLeave={() => mouseLeave(index)}
+							className='text-gray-600 relative inline-block'>
+							<li>
 								<Link
 									href={list.href}
 									className={
@@ -90,51 +106,29 @@ const DesktopNav = ({
 									{list.name}
 								</Link>
 							</li>
-						);
-					} else {
-						return (
 							<div
-								key={index}
-								onMouseEnter={() => mouseEnter(index)}
-								onMouseLeave={() => mouseLeave(index)}
-								className='text-gray-600 relative inline-block'>
-								<li>
-									<Link
-										href={list.href}
-										className={
-											pathName === list.href
-												? `text-white`
-												: `text-gray-600`
-										}>
-										{list.name}
-									</Link>
-								</li>
-								<div
-									className='absolute flex flex-col gap-2 w-40 justify-center'
-									style={
-										isEnter
-											? {
-													backgroundColor: 'black',
-											  }
-											: { backgroundColor: 'unset' }
-									}>
-									{isEnter[index]
-										? list.child.map(
-												(child, childIndex) => (
-													<Link
-														className='z-[100] p-2 box-border hover:text-white'
-														key={childIndex}
-														href={child.href}>
-														{child.name}
-													</Link>
-												)
-										  )
-										: null}
-								</div>
+								className='absolute flex flex-col gap-2 w-40 justify-center'
+								style={
+									isEnter
+										? {
+												backgroundColor: 'black',
+										  }
+										: { backgroundColor: 'unset' }
+								}>
+								{isEnter[index]
+									? list.child.map((child, childIndex) => (
+											<Link
+												className='z-[100] p-2 box-border hover:text-white'
+												key={childIndex}
+												href={child.href}>
+												{child.name}
+											</Link>
+									  ))
+									: null}
 							</div>
-						);
-					}
-				})}
+						</div>
+					)
+				)}
 			</ul>
 			<button
 				className='flex lg:hidden text-gray-600 items-center'
@@ -153,6 +147,7 @@ const MobileNav = ({
 	pathName: string;
 	isActive: boolean;
 }) => {
+	const [isSummary, setSummary] = React.useState<Array<boolean>>([]);
 	React.useEffect(() => {
 		if (isActive) {
 			document.body.style.overflow = 'hidden';
@@ -163,6 +158,13 @@ const MobileNav = ({
 	function handleClick() {
 		setActive((prev) => !prev);
 	}
+	function handleClickSummary(index: number) {
+		setSummary((prev) => {
+			let arr = [...prev];
+			arr[index] = arr[index] ? false : true;
+			return arr;
+		});
+	}
 	return (
 		<header className='h-screen bg-black p-2 text-gray-600 fixed w-screen overflow-hidden z-50'>
 			<nav>
@@ -172,21 +174,51 @@ const MobileNav = ({
 					<IoMdClose size={'25px'} />
 				</button>
 				<ul className='pl-3 pr-3 text-base flex flex-col gap-1 uppercase'>
-					{navlist.map((list, index) => (
-						<li
-							key={index}
-							className={
-								pathName === list.href
-									? 'text-white font-bold'
-									: 'text-gray-600'
-							}
-							onClick={handleClick}>
-							<Link href={list.href}>{list.name}</Link>
-							{pathName === list.href ? (
-								<span className='block h-[1px] bg-white w-full'></span>
-							) : null}
-						</li>
-					))}
+					{navlist.map((list, index) =>
+						!list.child ? (
+							<li
+								key={index}
+								className={
+									pathName === list.href
+										? 'text-white font-bold'
+										: 'text-gray-600'
+								}
+								onClick={handleClick}>
+								<Link href={list.href}>{list.name}</Link>
+								{pathName === list.href ? (
+									<span className='block h-[1px] bg-white w-full'></span>
+								) : null}
+							</li>
+						) : (
+							<div
+								key={index}
+								onClick={() => handleClickSummary(index)}
+								className='flex flex-col'>
+								<Link
+									href={list.href}
+									className={
+										pathName === list.href
+											? 'text-white font-bold'
+											: 'text-gray-600'
+									}>
+									{list.name}
+								</Link>
+								{pathName === list.href ? (
+									<span className='block h-[1px] bg-white w-full'></span>
+								) : null}
+								{isSummary[index]
+									? list.child.map((child, childIndex) => (
+											<Link
+												href={child.href}
+												key={childIndex}
+												className='text-sm pt-2 hover:text-white'>
+												{child.name}
+											</Link>
+									  ))
+									: null}
+							</div>
+						)
+					)}
 				</ul>
 			</nav>
 			<Footer isFooter={false} />
